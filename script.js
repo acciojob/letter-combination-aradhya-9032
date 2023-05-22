37cdb55000783fbc012e92d819a6c52ef512acc5
Aradhya <120170628+aradhya-9032@users.noreply.github.com>
2023-05-22 19:52:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,41 +6,45 @@
 
 
 function letterCombinations(digits) {
-  if (digits.length === 0) {
-    return []; // Return an empty array for an empty digit string
-  }
-
-  const digitToLetters = {
-    '2': 'abc',
-    '3': 'def',
-    '4': 'ghi',
-    '5': 'jkl',
-    '6': 'mno',
-    '7': 'pqrs',
-    '8': 'tuv',
-    '9': 'wxyz',
+  const mapping = {
+    0: "0",
+    1: "1",
+    2: "abc",
+    3: "def",
+    4: "ghi",
+    5: "jkl",
+    6: "mno",
+    7: "pqrs",
+    8: "tuv",
+    9: "wxyz",
   };
-
-  const combinations = ['']; // Start with an empty combination
-
-  for (const digit of digits) {
-    const letters = digitToLetters[digit];
-    const newCombinations = [];
-
-    for (const combination of combinations) {
-      for (const letter of letters) {
-        newCombinations.push(combination + letter);
-      }
-    }
-
-    combinations.length = 0; // Clear the existing combinations
-    combinations.push(...newCombinations); // Add new combinations
+  
+  const combinations = [];
+  
+  if (digits.length === 0) {
+    return combinations;
   }
+  
+  backtrack("", digits);
+  
+  return combinations;
 
-  return combinations.sort(); // Sort the combinations lexicographically
+  function backtrack(current, remaining) {
+    if (remaining.length === 0) {
+      combinations.push(current);
+      return;
+    }
+    
+    const digit = remaining[0];
+    const letters = mapping[digit];
+    
+    for (let i = 0; i < letters.length; i++) {
+      const letter = letters[i];
+      backtrack(current + letter, remaining.slice(1));
+    }
+  }
 }
 
 // Example usage
-const digits = '23';
-const result = letterCombinations(digits);
-console.log(result);
+// console.log(letterCombinations("23")); // Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
+
