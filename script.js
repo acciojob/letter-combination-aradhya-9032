db34e8bc7a8013c6011efef6438d17d18b527f55
Aradhya <120170628+aradhya-9032@users.noreply.github.com>
2024-01-25 12:29:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
-let com = [0,1'abc','def','ghi','jkl','mno','pqrs','tuv','wxyz'];
+let com = [0, 1, 'abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz'];
 
-function f(ques,comb,res){
-	if(ques.length==0){
-		res.push(comb);
-		return;
-	}
-	let dig = parseInt(ques[0]);
-	let curStr = com[dig];
-	for(let i=0;i<curStr.length;i++){
-		let curChar = curStr[i];
-		f(ques,substring(1),comb+curChar,res);
-	}
+function f(ques, comb, res) {
+    if (ques.length === 0) {
+        res.push(comb);
+        return;
+    }
+    let dig = parseInt(ques[0]);
+    let curStr = com[dig];
+    for (let i = 0; i < curStr.length; i++) {
+        let curChar = curStr[i];
+        f(ques.substring(1), comb + curChar, res);
+    }
 }
-function letterCombinations(input_digit){
-	let res=[];
-	f(input_digit,"",res);
-	return res;
+
+function letterCombinations(input_digit) {
+    let res = [];
+    if (input_digit.length > 0) {
+        f(input_digit, '', res);
+    }
+    return res;
 }
+
+// Example usage
+let result = letterCombinations("23");
+console.log(result);
+
 module.exports =letterCombinations;
